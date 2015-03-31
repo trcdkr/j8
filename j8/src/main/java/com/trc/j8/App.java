4524cef5ebd7f76a62b89070ea4f804b4368ef3f
trcdkr <trcdkr@gmail.com>
2015-03-31 18:08:56 +0300

    mvn

---
+++ j8/src/main/java/com/trc/j8/App.java
@@ -0,0 +1,13 @@
+package com.trc.j8;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
